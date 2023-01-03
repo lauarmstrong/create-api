7aa2f408a239d9cf786b68cc6bb559962f669a15
Lauren Armstrong <lauren.armstrong@vaultplatform.com>
2023-01-03 14:02:54 +0000

    some changes

--- README.md
+++ README.md
@@ -23,16 +23,15 @@ This application makes use of the following libraries:
 
 Add a .env file with the following variables:
 
-POSTGRES_HOST=127.0.0.1
+POSTGRES_HOST=my-postgres
+DB_PORT=3000
 POSTGRES_DB = products_dev
 POSTGRES_USER = postgres
 POSTGRES_PASSWORD = password123
 ENV = DEV
 POSTGRES_TEST_DB = products_test
-BCRYPT_PASSWORD = (your-very-secret-password)
+BCRYPT_PASSWORD = very-secret-password
 SALT_ROUNDS = 10
-TOKEN_SECRET=(your_token)
-PORT=3000
 
 ## Database setup
 
@@ -45,7 +44,7 @@ In the .env file, change:
 
 POSTGRES_HOST=127.0.0.1 POSTGRES_USER=postgres
 
-run: docker build -t sql-express-products . and docker compose up, then navigate to http://localhost:3000
+run: docker build -t sql-express-products . and docker-compose up, then navigate to http://localhost:3000
 
 ## Starting the App
 
--- dist/server.js
+++ dist/server.js
@@ -2,21 +2,21 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-exports.__esModule = true;
-var express_1 = __importDefault(require("express"));
-var body_parser_1 = __importDefault(require("body-parser"));
-var products_1 = __importDefault(require("./handlers/products"));
-var users_1 = __importDefault(require("./handlers/users"));
-var dashboardRoutes_1 = __importDefault(require("./handlers/dashboardRoutes"));
-var app = (0, express_1["default"])();
-var address = "0.0.0.0:3000";
-app.use(body_parser_1["default"].json());
-app.get("/", function (req, res) {
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const products_1 = __importDefault(require("./handlers/products"));
+const users_1 = __importDefault(require("./handlers/users"));
+const dashboardRoutes_1 = __importDefault(require("./handlers/dashboardRoutes"));
+const app = (0, express_1.default)();
+const address = "0.0.0.0:3000";
+app.use(body_parser_1.default.json());
+app.get("/", (req, res) => {
     res.send("Hello World!");
 });
-(0, products_1["default"])(app);
-(0, users_1["default"])(app);
-(0, dashboardRoutes_1["default"])(app);
-app.listen(3000, function () {
-    console.log("starting app on: ".concat(address));
+(0, products_1.default)(app);
+(0, users_1.default)(app);
+(0, dashboardRoutes_1.default)(app);
+app.listen(3000, () => {
+    console.log(`starting app on: ${address}`);
 });
