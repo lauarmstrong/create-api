c77bc938b97326939086c3521ba11b6a1dc3d9b5
Lauren Armstrong <lauren.armstrong@vaultplatform.com>
2023-01-03 16:29:26 +0000

    remove authentication for create user

--- src/handlers/users.ts
+++ src/handlers/users.ts
@@ -23,22 +23,6 @@ export const verifyAuthToken = (
   }
 };
 
-const authenticate = async (req: Request, res: Response) => {
-  const user: User = {
-    username: req.body.username,
-    password: req.body.password,
-  };
-  try {
-    const u = await store.authenticate(user.username, user.password);
-    var token = jwt.sign({ user: u }, process.env.TOKEN_SECRET as Secret);
-    res.json(token);
-  } catch (error) {
-    //Failed authentification error
-    res.status(401);
-    res.json(error);
-  }
-};
-
 const index = async (req: Request, res: Response) => {
   try {
     const users = await store.index();
@@ -65,8 +49,7 @@ const create = async (req: Request, res: Response) => {
     };
 
     const newUser = await store.create(user);
-    var token = jwt.sign({ user: newUser }, process.env.TOKEN_SECRET as Secret);
-    res.json(token);
+    res.json(newUser);
   } catch (error) {
     res.status(400);
     res.json(error);
@@ -79,20 +62,6 @@ const update = async (req: Request, res: Response) => {
     username: req.body.username,
     password: req.body.password,
   };
-  // Users can only edit their own information
-  // try {
-  //   const authorizationHeader: string = (req.headers.authorization as unknown) as string;
-  //   const token = authorizationHeader.split(" ")[1];
-  //   const decoded = jwt.verify(token, process.env.TOKEN_SECRET as Secret);
-  //   if (decoded.id !== user.id) {
-  //     throw new Error("User id does not match!");
-  //   }
-  // } catch (err) {
-  //   //Failed authentification error
-  //   res.status(401);
-  //   res.json(err);
-  //   return;
-  // }
 
   try {
     const updated = await store.create(user);
@@ -116,10 +85,9 @@ const user_routes = (app: express.Application) => {
   app.get("/users", index);
   app.get("/users/:id", show);
   //Only logged-in users can create, update or delete a user
-  app.post("/users", verifyAuthToken, create);
+  app.post("/users", create);
   app.put("/users/:id", update);
-  app.delete("./users/:id", verifyAuthToken, destroy);
-  app.post("/authenticate", authenticate);
+  app.delete("./users/:id", destroy);
 };
 
 export default user_routes;
